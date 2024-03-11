512d9dff1d47d833e70c67605ca83f8edb9e6edf
Kirill Rekhov <krekhov.dev@gmail.com>
2024-03-11 23:41:31 +0300

    mygit.sh: update script
    change 'public_projects' list

--- .local/bin/mygit.sh
+++ .local/bin/mygit.sh
@@ -78,7 +78,7 @@ case $1 in
 			fi
 		done
 
-		public_projects=(binout typp bootstrap-env dotfiles-debian glibc-with-shred)
+		public_projects=(binout chroot-deb-builder dotfiles-debian glibc-with-shred typp)
 		for name in ${public_projects[*]}; do
 			if [ ! -d $name ]; then
 				git clone git@github.com:iikrllx/$name
