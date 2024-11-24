0e58294b37bd3207e424d41cb88a8c88fd161c92
Kirill Rekhov <krekhov.dev@gmail.com>
2024-11-24 16:27:05 +0300

    add super convenient script for get source uris
    examples:
    $ source-uris.sh /bin/bash
    $ source-uris.sh scp

---
+++ .local/bin/source-uris.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Get source uris.
+# Examples:
+# $ source-uris.sh /bin/bash
+# $ source-uris.sh scp
+#
+
+if [ ! -z "$1" ]; then
+	cmdpath=$(which "$1" 2>/dev/null)
+
+	if [ ! -z "$cmdpath" ]; then
+		pack=$(dpkg -S "$cmdpath" 2>/dev/null | awk '{print $1}' | sed 's/:$//')
+	fi
+
+	if [ ! -z "$pack" ]; then
+		sudo apt-get source --print-uris "$pack" 2>&1 | awk '
+		/^NOTICE:/ {print $NF}
+		/http/ {
+			match($0, /(http[^[:space:]'\''"]+)/, url)
+			if (url[1] != "") print url[1]
+		}' | grep -v '^at:' | uniq
+	else
+		echo "not found name: $1"
+		exit 1
+	fi
+else
+	>&2 echo "usage: $ $(basename $0) <command>"
+	exit 1
+fi
