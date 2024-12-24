7ed54a7df17e980eadacc301215220cecad6d413
Kirill Rekhov <krekhov.dev@gmail.com>
2024-12-24 20:58:52 +0300

    add new script: apt-recent-installs.sh
    script parses the /var/log/apt/history.log file to extract
    and display installation dates and package lists for each
    install command executed.

---
+++ .local/bin/apt-recent-installs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#
+# Script parses the /var/log/apt/history.log file to extract and display
+# installation dates and package lists for each install command executed.
+#
+
+if [ ! -f /var/log/apt/history.log ]; then
+	echo "File /var/log/apt/history.log not found"
+	exit 1
+fi
+
+awk '
+	/Start-Date:/ {
+		date = $0
+	}
+
+	/Commandline:.*install/ {
+		sub(/Commandline:.*install /, "Commandline: ")
+		print date
+		print $0
+		print "--"
+	}
+' /var/log/apt/history.log
