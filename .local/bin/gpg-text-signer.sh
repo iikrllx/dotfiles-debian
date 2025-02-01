2e24a2f32eadc22d6bfc6d91fe51aa9734d4db4e
Kirill Rekhov <krekhov.dev@gmail.com>
2025-02-01 15:44:31 +0300

    update gpg-text-signer.sh

--- .local/bin/gpg-text-signer.sh
+++ .local/bin/gpg-text-signer.sh
@@ -10,7 +10,7 @@ cat << EOF > text
 
 
 ---
-Kirill Rekhov
+Regargs, Kirill Rekhov
 
 GPG Fingerprint:
 2640 769D FDA1 AAA0 F863  D1AE 5F2C 5905 519C E0A0
@@ -22,3 +22,4 @@ gpg --clearsign text
 shred -uzn 8 text
 cat text.asc | xsel -b -i
 shred -uzn 8 text.asc
+echo "paste signed text from clipboard"
