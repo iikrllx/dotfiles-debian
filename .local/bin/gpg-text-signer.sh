1641e2857462fb250ead438b07e5a69de4cbb092
Kirill Rekhov <krekhov.dev@gmail.com>
2025-02-04 19:02:22 +0300

    gpg-text-signer.sh: update, add detach sign with armor

--- .local/bin/gpg-text-signer.sh
+++ .local/bin/gpg-text-signer.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
 #
-# Automates the process of writing a message, signing it with GPG, and copying
-# the signed message to the clipboard.
+# Automates the process of writing a message, signing it with GPG, copying the
+# signed message to the clipboard, and securely deleting the temporary file.
+# The detached signature is saved as 'signature.asc' in the /tmp directory.
 #
 
 set -e
 
-cat << EOF > text
+text=$(mktemp)
+cat << EOF > $text
 
 
 ---
-Regargs, Kirill Rekhov
+Regards, Kirill Rekhov
 
 GPG Fingerprint:
 2640 769D FDA1 AAA0 F863  D1AE 5F2C 5905 519C E0A0
-
 EOF
 
-vim text
-gpg --clearsign text
-shred -uzn 8 text
-cat text.asc | xsel -b -i
-shred -uzn 8 text.asc
-echo "paste signed text from clipboard"
+vim $text
+gpg --detach-sign --armor -o ~/signature.asc $text
+cat $text | xsel -b -i
+shred -uzn 8 $text
+echo "> paste signed text from clipboard"
+echo "> attach signature.asc"
