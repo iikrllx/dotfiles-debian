69e0d979fb2fbe727c7c5a500e5099e03ac9a9b7
Kirill Rekhov <krekhov.dev@gmail.com>
2025-02-23 20:19:26 +0300

    add new script: wnpp-requested-github.sh

---
+++ .local/bin/wnpp-requested-github.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+a=$(mktemp)
+b=$(mktemp)
+
+cleanup() {
+	rm -f /tmp/rev "$a" "$b"
+}
+
+curl -s "https://www.debian.org/devel/wnpp/requested_byage" > "$a"
+
+grep 'href="https://bugs.debian.org/*' "$a" | \
+sed -n 's/.*href="https:\/\/bugs\.debian\.org\/\([0-9]*\)".*/\1/p' > "$b"
+tac "$b" > /tmp/rev
+
+trap 'cleanup; exit' SIGINT ERR
+trap 'cleanup' EXIT
+
+counter=0
+for n in $(cat /tmp/rev); do
+	if (( counter == 1000 )); then
+		break
+	fi
+
+	github=$(curl -s "https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=${n};msg=5" \
+	| grep -A 1 -E '\*? URL[^:]*:' | grep -Eo 'href="[^"]+"' | grep 'https://github.com' \
+	| awk -F'"' '{print $2}')
+
+	if [ ! -z "$github" ]; then
+		echo "https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=${n}"
+		echo -e "$github\n"
+	fi
+
+	counter=$((counter + 1))
+done
