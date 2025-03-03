b9a7f3aa43bc3c1e44d977ef12fced5f0ae3deea
Kirill Rekhov <krekhov.dev@gmail.com>
2025-03-03 17:16:42 +0300

    add new script: qemu-launcher.sh

---
+++ .local/bin/qemu-launcher.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# Launch QEMU with specified options.
+#
+
+usage()
+{
+if [ $1 -eq 1 ]; then
+        >&2 echo "Try '$(basename $0) --help' for more information."
+        exit 1
+else
+cat << EOF
+Usage: $(basename $0) [option]
+Launch QEMU with specified options.
+
+  [option]
+  -qc, --qemu-cdrom      launch QEMU using a specified CD-ROM ISO.
+  -qd, --qemu-tempdisk   create a temporary disk and launch QEMU with it.
+  -h                     show this help and exit
+
+EOF
+
+exit 0
+fi
+}
+
+case "$1" in
+'-qc'|'--qemu-cdrom')
+	qemu-system-x86_64 -boot d -cdrom "$1" -m 2048
+	;;
+
+'-qd'|'--qemu-tempdisk')
+	f=$(mktemp)
+	qemu-img create -f qcow2 "/tmp/$f.img" 12G
+	qemu-system-x86_64 -boot d -cdrom "$1" -m 2048 -hda "/tmp/$f.img"
+	;;
+
+'-h'|'--help')
+	usage 0
+	;;
+
+*)
+	usage 1
+	;;
+esac
